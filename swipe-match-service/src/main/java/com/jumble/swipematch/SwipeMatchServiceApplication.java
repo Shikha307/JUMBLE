21832c73bfda8c344ead422d2da3f1b2a0127f74
Saiesh Kaul <saieshkaul01@gmail.com>
2026-03-07 15:13:20 -0700

    swipe-engine

---
+++ swipe-match-service/src/main/java/com/jumble/swipematch/SwipeMatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jumble.swipematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeMatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipeMatchServiceApplication.class, args);
+	}
+
+}
