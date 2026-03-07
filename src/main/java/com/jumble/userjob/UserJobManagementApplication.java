e257eaba6affcf56ed7911fbb19919b3f6955966
Jai Damani <damani.jai73@gmail.com>
2026-03-07 13:31:39 -0700

    dummy springboot commit

---
+++ src/main/java/com/jumble/userjob/UserJobManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jumble.userjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJobManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJobManagementApplication.class, args);
+	}
+
+}
