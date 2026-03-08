8f9d59e3f9106d4edb2efb2187bac4c1ef88f5ed
Jai Damani <damani.jai73@gmail.com>
2026-03-07 17:38:25 -0700

    new apis added

--- src/main/java/com/jumble/userjob/candidate/service/RecruiterService.java
+++ src/main/java/com/jumble/userjob/candidate/service/RecruiterService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 
 import com.jumble.userjob.candidate.model.Recruiter;
 import com.jumble.userjob.candidate.repository.RecruiterRepository;
+import com.jumble.userjob.job.model.Job;
+import com.jumble.userjob.job.repository.JobRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,11 +17,18 @@ public class RecruiterService {
     @Autowired
     private RecruiterRepository recruiterRepository;
 
+    @Autowired
+    private JobRepository jobRepository;
+
     public Recruiter createRecruiter(Recruiter recruiter) {
         return recruiterRepository.save(recruiter);
     }
 
     public Optional<Recruiter> getRecruiterById(String id) {
         return recruiterRepository.findById(id);
+    }
+
+    public List<Job> getJobsByRecruiterId(String id) {
+        return jobRepository.findByRecruiterId(id);
     }
 }
