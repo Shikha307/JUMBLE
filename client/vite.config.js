df0b8af7a6a025fef0fba2224aeb922495e3010c
Shrisha Shriram Kulkarni <shku6698@colorado.edu>
2026-04-13 17:33:33 -0600

    fix: explicitly add Cloud Run host to Vite allowedHosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: true,
+    allowedHosts: ['client-70755451505.us-central1.run.app'],
   },
 })
 
